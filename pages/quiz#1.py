0394df96823eb0e470247828fab5f692ad10588a
pauline-crypto <paulinejoyoleobautista05@gmail.com>
2023-03-14 12:07:37 +0800

    message

--- pages/quiz#1.py
+++ pages/quiz#1.py
@@ -2,13 +2,17 @@ import numpy as np
 import cv2
 import streamlit as st
 
-def read_image(filename):
-    img = cv2.imread(filename)
-    if img is None:
-        st.error("Error: Image file not found or could not be read.")
+def read_image(file):
+    try:
+        img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        if img is None:
+            st.error("Error: Image file not found or could not be read.")
+            return None
+        else:
+            return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
         return None
-    else:
-        return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 def translate_image(img, tx_new, ty_new):
     rows, cols = img.shape[:2]
@@ -16,24 +20,28 @@ def translate_image(img, tx_new, ty_new):
     translated_img = cv2.warpAffine(img, M, (cols, rows))
     return translated_img
 
-def main(): 
-    img = read_image("original.jpg")
-    if img is None:
-        return
+def main():
+    st.title("Image Translation")
 
-    tx, ty = 0, 0
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        img = read_image(uploaded_file)
+        if img is not None:
+            st.image(img, caption="Original Image")
 
-    while True:
-        tx_new = st.text_input("Enter new x coordinate or press 1 to exit: ")
-        if tx_new == "1":
-            st.write("Thank you!")
-            break
+            tx, ty = 0, 0
 
-        ty_new = st.text_input("Enter new y coordinate: ")
-        tx += int(tx_new)
-        ty += int(ty_new)
-        transformed_img = translate_image(img, tx, ty)
-        st.image(transformed_img)
+            while True:
+                tx_new = st.text_input("Enter new x coordinate or press 1 to exit: ")
+                if tx_new == "1":
+                    st.write("Thank you!")
+                    break
+
+                ty_new = st.text_input("Enter new y coordinate: ")
+                tx += int(tx_new)
+                ty += int(ty_new)
+                transformed_img = translate_image(img, tx, ty)
+                st.image(transformed_img, caption=f"Translated Image ({tx}, {ty})")
 
 if __name__ == '__main__':
     main()
